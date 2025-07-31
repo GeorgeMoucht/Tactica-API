1af401e875c9f4f4a595520966a7f35554119ec0
GeorgeMoucht <giorgosmoucht@gmail.com>
2025-07-31 23:26:49 +0300

    Delete project to fresh start.

--- Tactica.API/DTOs/Auth/RegisterRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace Tactica.API.DTOs.Auth;
-
-public class RegisterRequest
-{
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
